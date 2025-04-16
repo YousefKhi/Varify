054429b7ce33600137efab6999f114aa4167c461
Yousef Khirallah <Yousefkhi@outlook.com>
2025-04-15 23:11:01 -0300

    test

--- next.config.js
+++ next.config.js
@@ -9,6 +9,22 @@ const nextConfig = {
       "logos-world.net",
     ],
   },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all API routes
+        source: "/api/:path*",
+        headers: [
+          // Allow requests from any origin (replace '*' with your specific domain in production for better security)
+          { key: "Access-Control-Allow-Origin", value: "*" }, 
+          // Allow specific methods
+          { key: "Access-Control-Allow-Methods", value: "GET, POST, OPTIONS" }, 
+          // Allow specific headers (you might need to adjust this based on your requests)
+          { key: "Access-Control-Allow-Headers", value: "Content-Type" }, 
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
