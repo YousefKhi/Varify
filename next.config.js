b76b1cf8a83d6d7057921010beead8c8e5ebf59f
Yousef Khirallah <Yousefkhi@outlook.com>
2025-04-15 22:37:30 -0300

    test

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true, // 👈 this tells Next.js to use /app as your router
-  },
   images: {
     domains: [
       "lh3.googleusercontent.com",
