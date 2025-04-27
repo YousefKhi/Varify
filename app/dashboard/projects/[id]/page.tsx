88248ca16471671ab18187aea93142b3f24de772
Yousef Khirallah <Yousefkhi@outlook.com>
2025-04-27 15:48:16 -0300

    test

--- app/dashboard/projects/[id]/page.tsx
+++ app/dashboard/projects/[id]/page.tsx
@@ -1,21 +1,23 @@
 import { notFound } from "next/navigation";
 import { createClient } from "@/libs/supabase/server";
 
+export const dynamic = "force-dynamic"; // <-- ADD THIS!!
+
 export default async function ProjectPage({ params }: { params: { id: string } }) {
   const supabase = createClient();
 
   const { data: project, error } = await supabase
-    .from("projects")
-    .select("id, name, created_at, site_url")
-    .eq("id", params.id) // <--- IMPORTANT
+    .from('projects')
+    .select('id, name, created_at, site_url')
+    .eq('id', params.id)
     .single();
 
-  console.log("params.id", params.id);
-  console.log("project", project);
-  console.log("error", error);
+  console.log('params.id', params.id);
+  console.log('project', project);
+  console.log('error', error);
 
   if (!project || error) {
-    notFound(); // <--- if no project found or error, show 404
+    notFound();
   }
 
   return (
