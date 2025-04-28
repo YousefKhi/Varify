e874917cc05149ef6123fa284ce48da70444432f
Yousef Khirallah <Yousefkhi@outlook.com>
2025-04-28 12:45:33 -0300

    test

--- app/dashboard/projects/[id]/page.tsx
+++ app/dashboard/projects/[id]/page.tsx
@@ -16,6 +16,7 @@ export default async function ProjectPage({ params }: { params: { id: string } }
   console.log('project', project);
   console.log('error', error);
 
+
   if (!project || error) {
     notFound();
   }
